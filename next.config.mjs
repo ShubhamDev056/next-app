01c319e644620d6190144ec65e66fa42a14127a8
Shubham Kumar <056shubham@gmail.com>
2024-12-26 14:55:58 +0530

    updated path in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: "./", // Ensures assets (like CSS) are served relative to the app
+  basePath: "", // Optional: Set if app is served from a subdirectory
+};
 
 export default nextConfig;
